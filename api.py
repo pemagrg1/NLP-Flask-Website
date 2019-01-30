7a87deafd459b3004d0a38f20bcdc9c09ceb1029
Pema <pema.gurung@ekbana.com>
2019-01-30 15:57:08 +0545

    adding the whole structure

---
+++ api.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template,jsonify
+
+app = Flask(__name__)
+
+def do_something(text1,text2):
+   text1 = text1.upper()
+   text2 = text2.upper()
+   combine = text1 + text2
+   return combine
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/join', methods=['GET','POST'])
+def my_form_post():
+    text1 = request.form['text1']
+    word = request.args.get('text1')
+    text2 = request.form['text2']
+    combine = do_something(text1,text2)
+    result = {
+        "output": combine
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
