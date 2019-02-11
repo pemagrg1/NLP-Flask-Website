5ac17a2e720c230cbb1aa88770d1d4d8354932d5
Pema <pema.gurung@ekbana.com>
2019-02-11 11:18:56 +0545

    preprocess template and api

--- api.py
+++ api.py
@@ -1,12 +1,8 @@
 from flask import Flask, request, render_template,jsonify
+import nltk
+from autocorrect import spell
 
 app = Flask(__name__)
-
-def do_something(text1,text2):
-   text1 = text1.upper()
-   text2 = text2.upper()
-   combine = text1 + text2
-   return combine
 
 @app.route('/')
 @app.route('/home')
@@ -28,25 +24,121 @@ def pre_process():
 def others():
     return render_template('others.html')
 
-@app.route('/join', methods=['GET','POST'])
-def my_form_post():
-    text1 = request.form['text1']
-    word = request.args.get('text1')
-    text2 = request.form['text2']
-    combine = do_something(text1,text2)
+
+@app.route('/lower', methods=["GET", "POST"])
+def lower_case():
+    text1 = request.form['text']
+    word = text1.lower()
     result = {
-        "output": combine
+        "result": word
     }
     result = {str(key): value for key, value in result.items()}
     return jsonify(result=result)
 
-@app.route('/lower', methods=["POST"])
-def lower_case():
-    text1 = request.form['text1']
-    print(text1)
-    word = text1.lower()
+@app.route('/sent_tokenize', methods=["GET", "POST"])
+def sent_tokenize():
+    text = request.form['text']
+    sent_tokenize = nltk.sent_tokenize(text)
     result = {
-        "lower_word": word
+        "result": str(sent_tokenize) #remove str() if you want the output as list
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.route('/word_tokenize', methods=["GET", "POST"])
+def word_tokenize():
+    text = request.form['text']
+    word_tokenize = nltk.word_tokenize(text)
+    result = {
+        "result": str(word_tokenize) #remove str() if you want the output as list
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.route('/spell_check', methods=["GET", "POST"])
+def spell_check():
+    text = request.form['text']
+    spells = [spell(w) for w in (nltk.word_tokenize(text))]
+    result = {
+        "result": " ".join(spells)
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.route('/lemmatize', methods=["GET", "POST"])
+def lemmatize():
+    from nltk.stem import WordNetLemmatizer
+    wordnet_lemmatizer = WordNetLemmatizer()
+
+    text = request.form['text']
+    word_tokens = nltk.word_tokenize(text)
+    lemmatized_word = [wordnet_lemmatizer.lemmatize(word) for word in
+                       word_tokens]
+    result = {
+        "result": " ".join(lemmatized_word)
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.route('/stemming', methods=["GET", "POST"])
+def stemming():
+    from nltk.stem import SnowballStemmer
+    snowball_stemmer = SnowballStemmer('english')
+
+    text = request.form['text']
+    word_tokens = nltk.word_tokenize(text)
+    stemmed_word = [snowball_stemmer.stem(word) for word in word_tokens]
+    result = {
+        "result": " ".join(stemmed_word)
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.route('/remove_tags', methods=["GET", "POST"])
+def remove_tags():
+    import re
+    text = request.form['text']
+    cleaned_text = re.sub('<[^<]+?>', '', text)
+    result = {
+        "result": cleaned_text
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+
+@app.route('/remove_numbers', methods=["GET", "POST"])
+def remove_numbers():
+    text = request.form['text']
+    remove_num = ''.join(c for c in text if not c.isdigit())
+    result = {
+        "result": remove_num
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.route('/remove_punct', methods=["GET", "POST"])
+def remove_punct():
+    from string import punctuation
+    def strip_punctuation(s):
+        return ''.join(c for c in s if c not in punctuation)
+
+    text = request.form['text']
+    text = strip_punctuation(text)
+    result = {
+        "result": text
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.route('/remove_stopwords', methods=["GET", "POST"])
+def remove_stopwords():
+    from nltk.corpus import stopwords
+    stopword = stopwords.words('english')
+    text = request.form['text']
+    word_tokens = nltk.word_tokenize(text)
+    removing_stopwords = [word for word in word_tokens if word not in stopword]
+    result = {
+        "result": " ".join(removing_stopwords)
     }
     result = {str(key): value for key, value in result.items()}
     return jsonify(result=result)
